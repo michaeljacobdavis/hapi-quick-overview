da9dc4caeb11d4e9ff3576ccff446f4eecda4d55
Mike Davis <mike@michaeljacobdavis.com>
2015-07-13 21:26:47 -0700

    Added lout

--- example/index.js
+++ example/index.js
@@ -3,17 +3,11 @@ var server = new Hapi.Server();
 
 server.connection({ port: 3000 });
 
-server.register({register: require('./service-plugin')}, function (err) {
-    if (err) {
-        console.error('Failed to load service plugin:', err);
-    }
-});
+server.register({register: require('./service-plugin')}, function (err) {});
 
-server.register({register: require('./view-plugin')}, function (err) {
-    if (err) {
-        console.error('Failed to load views plugin:', err);
-    }
-});
+server.register({register: require('./view-plugin')}, function (err) {});
+
+server.register({ register: require('lout') }, function(err) {});
 
 server.start(function () {
     console.log('Server running at:', server.info.uri);
